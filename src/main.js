0f7f6a34cd4d131a7b9207693a0f3475de8aa66a
megametango <megame.amy@gmail.com>
2026-09-04 22:15:06 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import tokens from './tokens.json'
+
+document.documentElement.style.setProperty('--color-primary', tokens.color.primary)
+document.documentElement.style.setProperty('--color-background', tokens.color.background)
+document.documentElement.style.setProperty('--color-text', tokens.color.text)
